2f519f88c3097abcac8ae9edae2ee16fc44614d9
AhmedPeaRL <89841973+AhmedPeaRL@users.noreply.github.com>
2025-12-29 14:28:10 +0200

    Create existential_csv_seed.py

---
+++ existential_csv_seed.py
@@ -0,0 +1,9 @@
+This generator outputs incomplete CSV-like structures.
+
+They are:
+- not meaningful alone
+- not validated
+- not intended for release
+
+Their only function:
+to expose structure without obligation.
